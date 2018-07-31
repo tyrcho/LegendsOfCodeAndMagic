b8cc24cc7b92cd995c33ffdf99355f891ec493c8
Michel Daviot <michel@daviot.info>
2018-08-01 00:45:56 +0200

    batch games or server

---
+++ src/main/java/SingleGame.java
@@ -0,0 +1,21 @@
+import com.codingame.gameengine.runner.MultiplayerGameRunner;
+
+import java.util.Random;
+
+public class SingleGame {
+  public static void main(String[] args) throws Exception {
+    MultiplayerGameRunner gameRunner = new MultiplayerGameRunner();
+    System.setProperty("league.level", "4");
+    if ((new Random()).nextBoolean()) {
+      BatchGames.addSubmitted(gameRunner);
+      BatchGames.addIde(gameRunner);
+    } else {
+      BatchGames.addIde(gameRunner);
+      BatchGames.addSubmitted(gameRunner);
+      gameRunner.start();
+    }
+  }
+
+
+
+}
