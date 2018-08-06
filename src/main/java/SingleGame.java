5445b561216d439f506a666b02bf84ecc0de0b25
Michel Daviot <michel@daviot.info>
2018-08-07 00:07:50 +0200

    :bug: Fix stupid {} bug

--- src/main/java/SingleGame.java
+++ src/main/java/SingleGame.java
@@ -1,18 +1,16 @@
 import com.codingame.gameengine.runner.MultiplayerGameRunner;
 
-import java.util.Random;
-
 public class SingleGame {
 
   public static void main(String[] args) {
     MultiplayerGameRunner gameRunner = new MultiplayerGameRunner();
     System.setProperty("league.level", "4");
-    if ((new Random()).nextBoolean()) {
-      PrepareAgent.addIdeLast(gameRunner);
-    } else {
-      PrepareAgent.addIdeFirst(gameRunner);
-      gameRunner.start();
-    }
+//    if ((new Random()).nextBoolean()) {
+    PrepareAgent.addIdeLast(gameRunner);
+//    } else {
+//      PrepareAgent.addIdeFirst(gameRunner);
+    gameRunner.start();
+
   }
 
 
